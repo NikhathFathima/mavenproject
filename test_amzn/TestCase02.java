85dc634ac4283419cf2c32febb397e3bb391c606
NikhathFathima <109644085+NikhathFathima@users.noreply.github.com>
2025-01-22 00:53:56 +0530

    Add files via upload

---
+++ test_amzn/TestCase02.java
@@ -0,0 +1,39 @@
+package test_amzn;
+
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import source_amzn.HomePage;
+import source_amzn.Sign_In_Page;
+import source_amzn.Sign_In_Password_Page;
+
+//---------------------------------- Login-Search-LogOut -------------------------------------------------------------------
+
+
+public class TestCase02 extends LaunchQuit
+{
+	@Test()
+	public void Login_To_Amazon_Search_LogOut() throws InterruptedException
+	{
+		// ------------------- Sign In -----------------------------------------//
+		HomePage HomePage = new HomePage(driver);
+		HomePage.Ac_List(driver);
+		HomePage.Sign_In();
+		Sign_In_Page SingInPage = new Sign_In_Page(driver);
+		SingInPage.UserName();
+		SingInPage.Continue_Click();
+		Sign_In_Password_Page SignInPwPage = new Sign_In_Password_Page(driver);
+		SignInPwPage.Enter_Password();
+		SignInPwPage.SignIn_Submit();
+		// ------------------- Search -----------------------------------------//
+		HomePage.SearchProduct();
+		
+		// ------------------- Sign Out -----------------------------------------//
+		HomePage.Ac_List(driver);
+		HomePage.Sign_Out();
+		//--------------------Assert True if Sing Out successful --------------------------------//
+		Assert.assertEquals(SingInPage.UserName_Elmnt, false);
+		Reporter.log("----------Sign Out SuccessFull----------");
+	}
+}
